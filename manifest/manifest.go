764c255e0e513ba9b42910f1bd830c5e5585aec7
Lei Jitang <leijitang@huawei.com>
2016-09-05 16:18:19 +0800

    Update oci image spec mime type
    Signed-off-by: Lei Jitang <leijitang@huawei.com>

--- manifest/manifest.go
+++ manifest/manifest.go
@@ -28,9 +28,9 @@ const (
 	// OCIV1ImageManifestListMIMEType specifies the mediaType for an image manifest list.
 	OCIV1ImageManifestListMIMEType = "application/vnd.oci.image.manifest.list.v1+json"
 	// OCIV1ImageSerializationMIMEType is the mediaType used for layers referenced by the manifest.
-	OCIV1ImageSerializationMIMEType = "application/vnd.oci.image.serialization.rootfs.tar.gzip"
+	OCIV1ImageSerializationMIMEType = "application/vnd.oci.image.layer.tar+gzip"
 	// OCIV1ImageSerializationConfigMIMEType specifies the mediaType for the image configuration.
-	OCIV1ImageSerializationConfigMIMEType = "application/vnd.oci.image.serialization.config.v1+json"
+	OCIV1ImageSerializationConfigMIMEType = "application/vnd.oci.image.config.v1+json"
 )
 
 // DefaultRequestedManifestMIMETypes is a list of MIME types a types.ImageSource
