e928b402a5ab75b84b3f315effe65df8fa1b3774
Miloslav Trmač <mitr@redhat.com>
2017-01-20 07:54:47 +0100

    Drop unused constants from docker/reference.
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- docker/reference/reference.go
+++ docker/reference/reference.go
@@ -12,17 +12,6 @@ import (
 	"github.com/pkg/errors"
 )
 
-const (
-	// XDefaultTag defines the default tag used when performing images related actions and no tag or digest is specified
-	XDefaultTag = "latest"
-	// XDefaultHostname is the default built-in hostname
-	XDefaultHostname = "docker.io"
-	// XLegacyDefaultHostname is automatically converted to DefaultHostname
-	XLegacyDefaultHostname = "index.docker.io"
-	// XDefaultRepoPrefix is the prefix used for default repositories in default host
-	XDefaultRepoPrefix = "library/"
-)
-
 // XParseNamed parses s and returns a syntactically valid reference implementing
 // the Named interface. The reference must have a name, otherwise an error is
 // returned.
