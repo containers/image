97f94aa4f0edb754605f8b6e98d77bd330e99846
Giuseppe Scrivano <gscrivan@redhat.com>
2017-11-06 23:13:32 +0100

    ostree, source: copy files directly from the storage
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- ostree/ostree_src.go
+++ ostree/ostree_src.go
@@ -9,43 +9,46 @@ import (
 	"encoding/base64"
 	"fmt"
 	"io"
-	"io/ioutil"
-	"os"
 	"os/exec"
 	"strconv"
 	"strings"
 	"unsafe"
 
+	"github.com/containers/image/manifest"
+	"github.com/containers/image/types"
 	"github.com/containers/storage/pkg/ioutils"
-	"github.com/ostreedev/ostree-go/pkg/otbuiltin"
+	"github.com/opencontainers/go-digest"
+	glib "github.com/ostreedev/ostree-go/pkg/glibobject"
 	"github.com/pkg/errors"
 	"github.com/vbatts/tar-split/tar/asm"
 	"github.com/vbatts/tar-split/tar/storage"
-
-	"github.com/containers/image/manifest"
-	"github.com/containers/image/types"
-	"github.com/opencontainers/go-digest"
-	imgspecv1 "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
-// #cgo pkg-config: glib-2.0 gobject-2.0
+// #cgo pkg-config: glib-2.0 gobject-2.0 ostree-1
 // #include <glib.h>
 // #include <glib-object.h>
 // #include <gio/gio.h>
 // #include <stdlib.h>
+// #include <ostree.h>
+// #include <gio/ginputstream.h>
 import "C"
 
 func parseGVariantString(in string) string {
 	cstring := C.CString(in)
 	defer C.free(unsafe.Pointer(cstring))
 
-	ptr := (*C.char)(C.g_variant_get_string(C.g_variant_parse(nil, (*C.gchar)(cstring), nil, nil, nil), nil))
+	variant := C.g_variant_parse(nil, (*C.gchar)(cstring), nil, nil, nil)
+	defer C.g_variant_unref(variant)
+
+	ptr := (*C.char)(C.g_variant_get_string(variant, nil))
+
 	return C.GoString(ptr)
 }
 
 type ostreeImageSource struct {
 	ref    ostreeReference
 	tmpDir string
+	repo   *C.struct_OstreeRepo
 }
 
 // newImageSource returns an ImageSource for reading from an existing directory.
@@ -113,67 +116,151 @@ func (s *ostreeImageSource) GetManifest(instanceDigest *digest.Digest) ([]byte, 
 }
 
 func (s *ostreeImageSource) GetTargetManifest(digest digest.Digest) ([]byte, string, error) {
-	return nil, imgspecv1.MediaTypeImageManifest, nil
+	return nil, "", errors.New("manifest lists are not supported by this transport")
 }
 
-func (s *ostreeImageSource) checkout(dest string, checksum string) error {
-	opts := otbuiltin.NewCheckoutOptions()
-	opts.UserMode = true
-	commit := fmt.Sprintf("ociimage/%s", checksum)
-	return otbuiltin.Checkout(s.ref.repo, dest, commit, opts)
+func openRepo(path string) (*C.struct_OstreeRepo, error) {
+	var cerr *C.GError
+	cpath := C.CString(path)
+	defer C.free(unsafe.Pointer(cpath))
+	pathc := C.g_file_new_for_path(cpath)
+	defer C.g_object_unref(C.gpointer(pathc))
+	repo := C.ostree_repo_new(pathc)
+	r := glib.GoBool(glib.GBoolean(C.ostree_repo_open(repo, nil, &cerr)))
+	if !r {
+		C.g_object_unref(C.gpointer(repo))
+		return nil, glib.ConvertGError(glib.ToGError(unsafe.Pointer(cerr)))
+	}
+	return repo, nil
+}
+
+type ostreePathFileGetter struct {
+	repo       *C.struct_OstreeRepo
+	parentRoot *C.GFile
+}
+
+type ostreeReader struct {
+	stream *C.GFileInputStream
+}
+
+func (o ostreeReader) Close() error {
+	C.g_object_unref(C.gpointer(o.stream))
+	return nil
+}
+func (o ostreeReader) Read(p []byte) (int, error) {
+	var cerr *C.GError
+	instanceCast := C.g_type_check_instance_cast((*C.GTypeInstance)(unsafe.Pointer(o.stream)), C.g_input_stream_get_type())
+	stream := (*C.GInputStream)(unsafe.Pointer(instanceCast))
+
+	b := C.g_input_stream_read_bytes(stream, (C.gsize)(cap(p)), nil, &cerr)
+	if b == nil {
+		return 0, glib.ConvertGError(glib.ToGError(unsafe.Pointer(cerr)))
+	}
+	defer C.g_bytes_unref(b)
+
+	count := int(C.g_bytes_get_size(b))
+	if count == 0 {
+		return 0, io.EOF
+	}
+	data := (*[1 << 30]byte)(unsafe.Pointer(C.g_bytes_get_data(b, nil)))[:count:count]
+	copy(p, data)
+	return count, nil
+}
+
+func newOSTreePathFileGetter(repo *C.struct_OstreeRepo, commit string) (*ostreePathFileGetter, error) {
+	var cerr *C.GError
+	var parentRoot *C.GFile
+	cCommit := C.CString(commit)
+	defer C.free(unsafe.Pointer(cCommit))
+	if !glib.GoBool(glib.GBoolean(C.ostree_repo_read_commit(repo, cCommit, &parentRoot, nil, nil, &cerr))) {
+		return &ostreePathFileGetter{}, glib.ConvertGError(glib.ToGError(unsafe.Pointer(cerr)))
+	}
+
+	C.g_object_ref(C.gpointer(repo))
+
+	return &ostreePathFileGetter{repo: repo, parentRoot: parentRoot}, nil
+}
+
+func (o ostreePathFileGetter) Get(filename string) (io.ReadCloser, error) {
+	var file *C.GFile
+	if strings.HasPrefix(filename, "./") {
+		filename = filename[2:]
+	}
+	cfilename := C.CString(filename)
+	defer C.free(unsafe.Pointer(cfilename))
+
+	file = (*C.GFile)(C.g_file_resolve_relative_path(o.parentRoot, cfilename))
+
+	var cerr *C.GError
+	stream := C.g_file_read(file, nil, &cerr)
+	if stream == nil {
+		return nil, glib.ConvertGError(glib.ToGError(unsafe.Pointer(cerr)))
+	}
+
+	return &ostreeReader{stream: stream}, nil
+}
+
+func (o ostreePathFileGetter) Close() {
+	C.g_object_ref(C.gpointer(o.repo))
+	C.g_object_unref(C.gpointer(o.parentRoot))
+}
+
+func (s *ostreeImageSource) readSingleFile(commit, path string) (io.ReadCloser, error) {
+	getter, err := newOSTreePathFileGetter(s.repo, commit)
+	if err != nil {
+		return nil, err
+	}
+	defer getter.Close()
+
+	return getter.Get(path)
 }
 
 // GetBlob returns a stream for the specified blob, and the blob's size.
 func (s *ostreeImageSource) GetBlob(info types.BlobInfo) (io.ReadCloser, int64, error) {
 	blob := info.Digest.Hex()
+	branch := fmt.Sprintf("ociimage/%s", blob)
 
-	dir, err := ioutil.TempDir(s.tmpDir, string(info.Digest))
-	if err != nil {
-		return nil, 0, err
-	}
-
-	layer := fmt.Sprintf("%s/layer", dir)
-
-	err = s.checkout(layer, blob)
-	if err != nil {
-		os.RemoveAll(dir)
-		return nil, 0, err
+	if s.repo == nil {
+		repo, err := openRepo(s.ref.repo)
+		if err != nil {
+			return nil, 0, err
+		}
+		s.repo = repo
 	}
 
 	layerSize, err := s.getLayerSize(blob)
 	if err != nil {
-		os.RemoveAll(dir)
 		return nil, 0, err
 	}
 
 	tarsplit, err := s.getTarSplitData(blob)
 	if err != nil {
-		os.RemoveAll(dir)
 		return nil, 0, err
 	}
 
-	// if tarsplit is nil we are looking at the manifest.  Return directly the file in layer/content
+	// if tarsplit is nil we are looking at the manifest.  Return directly the file in /content
 	if tarsplit == nil {
-		stream, err := os.OpenFile(fmt.Sprintf("%s/content", layer), os.O_RDONLY, 0)
+		file, err := s.readSingleFile(branch, "/content")
 		if err != nil {
-			os.RemoveAll(dir)
 			return nil, 0, err
 		}
-		return ioutils.NewReadCloserWrapper(stream, func() error {
-			return os.RemoveAll(dir)
-		}), layerSize, nil
+		return file, layerSize, nil
 	}
 
 	mf := bytes.NewReader(tarsplit)
 	mfz, err := gzip.NewReader(mf)
 	if err != nil {
-		os.RemoveAll(dir)
 		return nil, 0, err
 	}
 	defer mfz.Close()
 	metaUnpacker := storage.NewJSONUnpacker(mfz)
 
-	ots := asm.NewOutputTarStream(storage.NewPathFileGetter(layer), metaUnpacker)
+	getter, err := newOSTreePathFileGetter(s.repo, branch)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	ots := asm.NewOutputTarStream(getter, metaUnpacker)
 
 	pipeReader, pipeWriter := io.Pipe()
 	gzipWriter := gzip.NewWriter(pipeWriter)
@@ -184,8 +271,8 @@ func (s *ostreeImageSource) GetBlob(info types.BlobInfo) (io.ReadCloser, int64, 
 	}()
 
 	rc := ioutils.NewReadCloserWrapper(pipeReader, func() error {
-		ots.Close()
-		return os.RemoveAll(dir)
+		getter.Close()
+		return ots.Close()
 	})
 	return rc, layerSize, nil
 }
