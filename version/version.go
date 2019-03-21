a2ecef50c1f931f02fbc84ac3a85abaa508314b9
Daniel J Walsh <dwalsh@redhat.com>
2019-03-21 17:39:25 -0400

    release v1.6.0
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- version/version.go
+++ version/version.go
@@ -4,14 +4,14 @@ import "fmt"
 
 const (
 	// VersionMajor is for an API incompatible changes
-	VersionMajor = 0
+	VersionMajor = 1
 	// VersionMinor is for functionality in a backwards-compatible manner
-	VersionMinor = 1
+	VersionMinor = 6
 	// VersionPatch is for backwards-compatible bug fixes
-	VersionPatch = 6
+	VersionPatch = 0
 
 	// VersionDev indicates development branch. Releases will be empty string.
-	VersionDev = "-dev"
+	VersionDev = ""
 )
 
 // Version is the specification version that the package types support.
