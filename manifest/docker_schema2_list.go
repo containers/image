fc304bbd7639689b03267cda9de39987b7491cdb
Jirka Chadima <chadima.jiri@gmail.com>
2020-01-09 14:01:34 +0100

    Improves error messages when matching image to a platform
    Signed-off-by: Jirka Chadima <chadima.jiri@gmail.com>

--- manifest/docker_schema2_list.go
+++ manifest/docker_schema2_list.go
@@ -110,7 +110,7 @@ func (list *Schema2List) ChooseInstance(ctx *types.SystemContext) (digest.Digest
 			}
 		}
 	}
-	return "", fmt.Errorf("no image found in image index for architecture %s, OS %s, Variant %s", wantedPlatforms[0].Architecture, wantedPlatforms[0].OS, wantedPlatforms[0].Variant)
+	return "", fmt.Errorf("no image found in manifest list for architecture %s, variant %s, OS %s", wantedPlatforms[0].Architecture, wantedPlatforms[0].Variant, wantedPlatforms[0].OS)
 }
 
 // Serialize returns the list in a blob format.
--- manifest/oci_index.go
+++ manifest/oci_index.go
@@ -100,7 +100,7 @@ func (index *OCI1Index) ChooseInstance(ctx *types.SystemContext) (digest.Digest,
 			return d.Digest, nil
 		}
 	}
-	return "", fmt.Errorf("no image found in image index for architecture %s, OS %s, Variant %s", wantedPlatforms[0].Architecture, wantedPlatforms[0].OS, wantedPlatforms[0].Variant)
+	return "", fmt.Errorf("no image found in manifest list for architecture %s, variant %s, OS %s", wantedPlatforms[0].Architecture, wantedPlatforms[0].Variant, wantedPlatforms[0].OS)
 }
 
 // Serialize returns the index in a blob format.
