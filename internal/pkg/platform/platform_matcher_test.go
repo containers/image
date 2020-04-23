a99f845c45d6702da4d3c0af61df2e52080c9257
Miloslav Trmač <mitr@redhat.com>
2020-04-23 16:19:23 +0200

    Combine the two TestWantedPlatforms* tests
    ... and make them table-driven.
    
    Does not change the test contents.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- internal/pkg/platform/platform_matcher_test.go
+++ internal/pkg/platform/platform_matcher_test.go
@@ -1,6 +1,7 @@
 package platform
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/containers/image/v5/types"
@@ -8,29 +9,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestWantedPlatformsCompatibility(t *testing.T) {
-	ctx := &types.SystemContext{
-		ArchitectureChoice: "arm",
-		OSChoice:           "linux",
-		VariantChoice:      "v6",
+func TestWantedPlatforms(t *testing.T) {
+	for _, c := range []struct {
+		ctx      types.SystemContext
+		expected []imgspecv1.Platform
+	}{
+		{ // ARM
+			types.SystemContext{ArchitectureChoice: "arm", OSChoice: "linux", VariantChoice: "v6"},
+			[]imgspecv1.Platform{
+				{OS: "linux", Architecture: "arm", Variant: "v6"},
+				{OS: "linux", Architecture: "arm", Variant: "v5"},
+			},
+		},
+		{ // Custom (completely unrecognized data)
+			types.SystemContext{ArchitectureChoice: "armel", OSChoice: "freeBSD", VariantChoice: "custom"},
+			[]imgspecv1.Platform{
+				{OS: "freeBSD", Architecture: "armel", Variant: "custom"},
+			},
+		},
+	} {
+		testName := fmt.Sprintf("%q/%q/%q", c.ctx.ArchitectureChoice, c.ctx.OSChoice, c.ctx.VariantChoice)
+		platforms, err := WantedPlatforms(&c.ctx)
+		assert.Nil(t, err, testName)
+		assert.Equal(t, c.expected, platforms, testName)
 	}
-	platforms, err := WantedPlatforms(ctx)
-	assert.Nil(t, err)
-	assert.Equal(t, []imgspecv1.Platform{
-		{OS: ctx.OSChoice, Architecture: ctx.ArchitectureChoice, Variant: "v6"},
-		{OS: ctx.OSChoice, Architecture: ctx.ArchitectureChoice, Variant: "v5"},
-	}, platforms)
-}
-
-func TestWantedPlatformsCustom(t *testing.T) {
-	ctx := &types.SystemContext{
-		ArchitectureChoice: "armel",
-		OSChoice:           "freeBSD",
-		VariantChoice:      "custom",
-	}
-	platforms, err := WantedPlatforms(ctx)
-	assert.Nil(t, err)
-	assert.Equal(t, []imgspecv1.Platform{
-		{OS: ctx.OSChoice, Architecture: ctx.ArchitectureChoice, Variant: ctx.VariantChoice},
-	}, platforms)
 }
