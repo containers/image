98fb94ed59753cdcca3f151ffc74dfe6e769e272
Miloslav Trmač <mitr@redhat.com>
2020-05-11 13:50:05 +0200

    v5.4.4
    * Bump to v5.5.0-dev again
    * Bump github.com/klauspost/compress from 1.10.3 to 1.10.4
    * Add User-Agent to oauth
    * Make the variant in TestChooseInstance data-controlled
    * Fix the docstring of WantedPlatforms
    * Introduce a variantOrder variable
    * Simplify the WantedPlatforms main loop
    * Simplify variant list preparation.
    * Simplify further
    * Simplify WantedPlatforms tests
    * Use quotes around variant name in errors
    * Bump github.com/containers/storage from 1.18.2 to 1.19.0
    * Bump github.com/klauspost/compress from 1.10.4 to 1.10.5
    * Bump github.com/vbauerster/mpb/v5 from 5.0.3 to 5.0.4
    * registries.conf: remove references on v1
    * Fix use of progressPool after it is closed.
    * OCI: Don't crash if OCI1Index.Manifests[].Platform is nil
    * Move format-indepenent private utilities to manifest/common.go
    * Split updatedSchema2MIMEType / updatedOCI1MIMEType from UpdatedLayerInfos
    * Factor out the common parts of MIME type update error messages
    * Make MIME type conversions table-driven
    * Factor out updatedMIMEType out of updatedSchema2MIMEType / updatedOCI1MIMEType
    * Inline updatedSchema2MIMEType and updatedOCI1MIMEType back into callers
    * Add internal/uploadreader.UploadReader
    * Use an UploadReader for layer upload.
    * Detect and report errors reported by the server on blob upload.
    * Fix debug logging on HTTP request error
    * Combine the two TestWantedPlatforms* tests
    * Improve handling of Variant: ""
    * Simplify and document MatchesPlatform
    * Recognize v8 as a valid ARM variant
    * Use the correct instance digest when uploading signatures using X-R-S-S
    * Use a by-value manifestDigest for the docker PutSignatures implementations
    * Remove registries.conf VERSION 2 references from man page
    * Bump github.com/sirupsen/logrus from 1.4.2 to 1.6.0
    * Bump github.com/containers/storage from 1.19.0 to 1.19.1
    * Don't change dockerImageSource.Reference() if a mirror is used
    * Commit Go-created go.mod updates
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- version/version.go
+++ version/version.go
@@ -6,12 +6,12 @@ const (
 	// VersionMajor is for an API incompatible changes
 	VersionMajor = 5
 	// VersionMinor is for functionality in a backwards-compatible manner
-	VersionMinor = 5
+	VersionMinor = 4
 	// VersionPatch is for backwards-compatible bug fixes
-	VersionPatch = 0
+	VersionPatch = 4
 
 	// VersionDev indicates development branch. Releases will be empty string.
-	VersionDev = "-dev"
+	VersionDev = ""
 )
 
 // Version is the specification version that the package types support.
