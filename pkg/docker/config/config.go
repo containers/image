e11bc539577c6dce114064564833ad256c21a028
Miloslav Trmač <mitr@redhat.com>
2021-07-27 19:52:43 +0200

    Don't build a map just to access one element
    Instead, just iterate and look for the key we want.
    
    NOTE: This might change behavior with ambiguous files,
    we now use the first matching entry, not the last one.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- pkg/docker/config/config.go
+++ pkg/docker/config/config.go
@@ -665,13 +665,10 @@ func findAuthentication(ref reference.Named, registry, path string, legacyFormat
 	// The docker.io registry still uses the /v1/ key with a special host name,
 	// so account for that as well.
 	registry = normalizeAuthFileKey(registry, legacyFormat)
-	normalizedAuths := map[string]dockerAuthConfig{}
 	for k, v := range auths.AuthConfigs {
-		normalizedAuths[normalizeAuthFileKey(k, legacyFormat)] = v
-	}
-
-	if val, exists := normalizedAuths[registry]; exists {
-		return decodeDockerAuth(val)
+		if normalizeAuthFileKey(k, legacyFormat) == registry {
+			return decodeDockerAuth(v)
+		}
 	}
 
 	return types.DockerAuthConfig{}, nil
