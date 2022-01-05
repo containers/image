71dfda12c94ef9919c9875860f9a7a88a2bae823
Miloslav Trmač <mitr@redhat.com>
2022-01-05 21:11:14 +0100

    Allow building the SIF transport on non-Linux systems
    It probabaly doesn't _work_ right now, at least macOS is
    missing a working fakeroot. We do intend to avoid the use of
    fakeroot eventually.
    
    (Adventurous experimenting developers might provide a no-op
    "fakeroot" script on most platforms.)
    
    Also, having the code compile on macOS significantly helps
    development.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- sif/transport.go
+++ sif/transport.go
@@ -1,6 +1,3 @@
-//go:build linux
-// +build linux
-
 package sifimage
 
 import (
--- transports/alltransports/sif.go
+++
@@ -1,9 +0,0 @@
-//go:build linux
-// +build linux
-
-package alltransports
-
-import (
-	// Register the sif transport
-	_ "github.com/containers/image/v5/sif"
-)
