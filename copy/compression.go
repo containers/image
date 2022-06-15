bf130bc85003e1ce9a16c1929e2a65c5e0700fa2
Miloslav Trmač <mitr@redhat.com>
2022-06-15 20:17:14 +0200

    Beautify blobPipelineDetectCompressionStep
    We can use shorter variable names in this short function now.
    
    Should not change behavior.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- copy/compression.go
+++ copy/compression.go
@@ -20,18 +20,19 @@ type bpDetectCompressionStepData struct {
 // Returns data for other steps.
 func blobPipelineDetectCompressionStep(stream *sourceStream, srcInfo types.BlobInfo) (bpDetectCompressionStepData, error) {
 	// This requires us to “peek ahead” into the stream to read the initial part, which requires us to chain through another io.Reader returned by DetectCompression.
-	compressionFormat, decompressor, detectCompressionFormatReader, err := compression.DetectCompressionFormat(stream.reader) // We could skip this in some cases, but let's keep the code path uniform
+	format, decompressor, reader, err := compression.DetectCompressionFormat(stream.reader) // We could skip this in some cases, but let's keep the code path uniform
 	if err != nil {
 		return bpDetectCompressionStepData{}, errors.Wrapf(err, "reading blob %s", srcInfo.Digest)
 	}
-	stream.reader = detectCompressionFormatReader
+	stream.reader = reader
+
 	res := bpDetectCompressionStepData{
 		isCompressed: decompressor != nil,
-		format:       compressionFormat,
+		format:       format,
 		decompressor: decompressor,
 	}
-	if expectedCompressionFormat, known := expectedCompressionFormats[stream.info.MediaType]; known && res.isCompressed && compressionFormat.Name() != expectedCompressionFormat.Name() {
-		logrus.Debugf("blob %s with type %s should be compressed with %s, but compressor appears to be %s", srcInfo.Digest.String(), srcInfo.MediaType, expectedCompressionFormat.Name(), compressionFormat.Name())
+	if expectedFormat, known := expectedCompressionFormats[stream.info.MediaType]; known && res.isCompressed && format.Name() != expectedFormat.Name() {
+		logrus.Debugf("blob %s with type %s should be compressed with %s, but compressor appears to be %s", srcInfo.Digest.String(), srcInfo.MediaType, expectedFormat.Name(), format.Name())
 	}
 	return res, nil
 }
