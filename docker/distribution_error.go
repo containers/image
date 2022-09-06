6cd738f44f44eef51e7b4d101f7fe7bc52c876d3
Miloslav Trmač <mitr@redhat.com>
2022-09-06 19:23:37 +0200

    Remove use of deprecated io/ioutil
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- docker/distribution_error.go
+++ docker/distribution_error.go
@@ -20,7 +20,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/docker/distribution/registry/api/errcode"
@@ -55,7 +54,7 @@ func (e *unexpectedHTTPResponseError) Error() string {
 
 func parseHTTPErrorResponse(statusCode int, r io.Reader) error {
 	var errors errcode.Errors
-	body, err := ioutil.ReadAll(r)
+	body, err := io.ReadAll(r)
 	if err != nil {
 		return err
 	}
