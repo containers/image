5434c330e5cda0d7910c395af8c93d60cd00bff5
Miloslav Trmač <mitr@redhat.com>
2023-01-05 02:20:53 +0100

    Return a generic signature.Signature from SignDockerManifestWithPrivateKeyFileUnstable
    ... instead of specifically signature.Sigstore.
    
    Should not change behavior, the only caller doesn't care.
    
    We will introduce an interface where we should allow creating all kinds of signature formats.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- signature/sigstore/sign.go
+++ signature/sigstore/sign.go
@@ -19,26 +19,26 @@ import (
 //
 // Yes, this returns an internal type, and should currently not be used outside of c/image.
 // There is NO COMITTMENT TO STABLE API.
-func SignDockerManifestWithPrivateKeyFileUnstable(m []byte, dockerReference reference.Named, privateKeyFile string, passphrase []byte) (signature.Sigstore, error) {
+func SignDockerManifestWithPrivateKeyFileUnstable(m []byte, dockerReference reference.Named, privateKeyFile string, passphrase []byte) (signature.Signature, error) {
 	privateKeyPEM, err := os.ReadFile(privateKeyFile)
 	if err != nil {
-		return signature.Sigstore{}, fmt.Errorf("reading private key from %s: %w", privateKeyFile, err)
+		return nil, fmt.Errorf("reading private key from %s: %w", privateKeyFile, err)
 	}
 	signer, err := loadPrivateKey(privateKeyPEM, passphrase)
 	if err != nil {
-		return signature.Sigstore{}, fmt.Errorf("initializing private key: %w", err)
+		return nil, fmt.Errorf("initializing private key: %w", err)
 	}
 
 	return signDockerManifest(m, dockerReference, signer)
 }
 
-func signDockerManifest(m []byte, dockerReference reference.Named, signer sigstoreSignature.Signer) (signature.Sigstore, error) {
+func signDockerManifest(m []byte, dockerReference reference.Named, signer sigstoreSignature.Signer) (signature.Signature, error) {
 	if reference.IsNameOnly(dockerReference) {
-		return signature.Sigstore{}, fmt.Errorf("reference %s can’t be signed, it has neither a tag nor a digest", dockerReference.String())
+		return nil, fmt.Errorf("reference %s can’t be signed, it has neither a tag nor a digest", dockerReference.String())
 	}
 	manifestDigest, err := manifest.Digest(m)
 	if err != nil {
-		return signature.Sigstore{}, err
+		return nil, err
 	}
 	// sigstore/cosign completely ignores dockerReference for actual policy decisions.
 	// They record the repo (but NOT THE TAG) in the value; without the tag we can’t detect version rollbacks.
@@ -46,14 +46,14 @@ func signDockerManifest(m []byte, dockerReference reference.Named, signer sigsto
 	payloadData := internal.NewUntrustedSigstorePayload(manifestDigest, dockerReference.String())
 	payloadBytes, err := json.Marshal(payloadData)
 	if err != nil {
-		return signature.Sigstore{}, err
+		return nil, err
 	}
 
 	// github.com/sigstore/cosign/internal/pkg/cosign.payloadSigner uses signatureoptions.WithContext(),
 	// which seems to be not used by anything. So we don’t bother.
 	signatureBytes, err := signer.SignMessage(bytes.NewReader(payloadBytes))
 	if err != nil {
-		return signature.Sigstore{}, fmt.Errorf("creating signature: %w", err)
+		return nil, fmt.Errorf("creating signature: %w", err)
 	}
 	base64Signature := base64.StdEncoding.EncodeToString(signatureBytes)
 
