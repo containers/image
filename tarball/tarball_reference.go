3220ac45c6ba64d7bd1b34e3fc6d22172b20d1b2
Miloslav Trmač <mitr@redhat.com>
2023-02-02 20:24:48 +0100

    Reformat import sections
    golangci-lint linter: gci
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- tarball/tarball_reference.go
+++ tarball/tarball_reference.go
@@ -9,7 +9,6 @@ import (
 	"github.com/containers/image/v5/docker/reference"
 	"github.com/containers/image/v5/internal/image"
 	"github.com/containers/image/v5/types"
-
 	imgspecv1 "github.com/opencontainers/image-spec/specs-go/v1"
 	"golang.org/x/exp/maps"
 )
