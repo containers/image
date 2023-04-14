65ffc9d827e609cd9fd4b7615c5bbed6938eff7e
Miloslav Trmač <mitr@redhat.com>
2023-04-14 20:35:29 +0200

    Use bytes.NewReader instead of bytes.NewBufferString
    We only want a Reader, so be more precise.
    
    More importantly, we want to remove all instances of the habit to use
    bytes.NewBuffer instead of bytes.NewReader (which can actually be unsafe),
    although that concern does not directly apply to these calls
    
    Should not change (test) behavior.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- docker/distribution_error_test.go
+++ docker/distribution_error_test.go
@@ -24,11 +24,11 @@ import (
 )
 
 func TestHandleErrorResponse401ValidBody(t *testing.T) {
-	json := "{\"errors\":[{\"code\":\"UNAUTHORIZED\",\"message\":\"action requires authentication\"}]}"
+	json := []byte("{\"errors\":[{\"code\":\"UNAUTHORIZED\",\"message\":\"action requires authentication\"}]}")
 	response := &http.Response{
 		Status:     "401 Unauthorized",
 		StatusCode: 401,
-		Body:       io.NopCloser(bytes.NewBufferString(json)),
+		Body:       io.NopCloser(bytes.NewReader(json)),
 	}
 	err := handleErrorResponse(response)
 
@@ -39,11 +39,11 @@ func TestHandleErrorResponse401ValidBody(t *testing.T) {
 }
 
 func TestHandleErrorResponse401WithInvalidBody(t *testing.T) {
-	json := "{invalid json}"
+	json := []byte("{invalid json}")
 	response := &http.Response{
 		Status:     "401 Unauthorized",
 		StatusCode: 401,
-		Body:       io.NopCloser(bytes.NewBufferString(json)),
+		Body:       io.NopCloser(bytes.NewReader(json)),
 	}
 	err := handleErrorResponse(response)
 
@@ -54,11 +54,11 @@ func TestHandleErrorResponse401WithInvalidBody(t *testing.T) {
 }
 
 func TestHandleErrorResponseExpectedStatusCode400ValidBody(t *testing.T) {
-	json := "{\"errors\":[{\"code\":\"DIGEST_INVALID\",\"message\":\"provided digest does not match\"}]}"
+	json := []byte("{\"errors\":[{\"code\":\"DIGEST_INVALID\",\"message\":\"provided digest does not match\"}]}")
 	response := &http.Response{
 		Status:     "400 Bad Request",
 		StatusCode: 400,
-		Body:       io.NopCloser(bytes.NewBufferString(json)),
+		Body:       io.NopCloser(bytes.NewReader(json)),
 	}
 	err := handleErrorResponse(response)
 
@@ -69,11 +69,11 @@ func TestHandleErrorResponseExpectedStatusCode400ValidBody(t *testing.T) {
 }
 
 func TestHandleErrorResponseExpectedStatusCode404EmptyErrorSlice(t *testing.T) {
-	json := `{"randomkey": "randomvalue"}`
+	json := []byte(`{"randomkey": "randomvalue"}`)
 	response := &http.Response{
 		Status:     "404 Not Found",
 		StatusCode: 404,
-		Body:       io.NopCloser(bytes.NewBufferString(json)),
+		Body:       io.NopCloser(bytes.NewReader(json)),
 	}
 	err := handleErrorResponse(response)
 
@@ -84,11 +84,11 @@ func TestHandleErrorResponseExpectedStatusCode404EmptyErrorSlice(t *testing.T) {
 }
 
 func TestHandleErrorResponseExpectedStatusCode404InvalidBody(t *testing.T) {
-	json := "{invalid json}"
+	json := []byte("{invalid json}")
 	response := &http.Response{
 		Status:     "404 Not Found",
 		StatusCode: 404,
-		Body:       io.NopCloser(bytes.NewBufferString(json)),
+		Body:       io.NopCloser(bytes.NewReader(json)),
 	}
 	err := handleErrorResponse(response)
 
@@ -102,7 +102,7 @@ func TestHandleErrorResponseUnexpectedStatusCode501(t *testing.T) {
 	response := &http.Response{
 		Status:     "501 Not Implemented",
 		StatusCode: 501,
-		Body:       io.NopCloser(bytes.NewBufferString("{\"Error Encountered\" : \"Function not implemented.\"}")),
+		Body:       io.NopCloser(bytes.NewReader([]byte("{\"Error Encountered\" : \"Function not implemented.\"}"))),
 	}
 	err := handleErrorResponse(response)
 
