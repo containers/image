9668bb98d67c59e93c205bfa63727c5593c32400
Miloslav Trmač <mitr@redhat.com>
2023-04-26 01:39:23 +0200

    Add unit tests for UpdateLayerInfos with encryption/decryption
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- manifest/oci_test.go
+++ manifest/oci_test.go
@@ -281,6 +281,68 @@ func TestOCI1UpdateLayerInfos(t *testing.T) {
 			},
 			expectedFixture: "ociv1.nondistributable.manifest.json",
 		},
+		{
+			name:          "uncompressed → gzip encrypted",
+			sourceFixture: "ociv1.uncompressed.manifest.json",
+			updates: []types.BlobInfo{
+				{
+					Digest:               "sha256:aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+					Size:                 32654,
+					Annotations:          map[string]string{"org.opencontainers.image.enc.…": "layer1"},
+					MediaType:            imgspecv1.MediaTypeImageLayer,
+					CompressionOperation: types.Compress,
+					CompressionAlgorithm: &compression.Gzip,
+					CryptoOperation:      types.Encrypt,
+				},
+				{
+					Digest:               "sha256:bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+					Size:                 16724,
+					Annotations:          map[string]string{"org.opencontainers.image.enc.…": "layer2"},
+					MediaType:            imgspecv1.MediaTypeImageLayer,
+					CompressionOperation: types.Compress,
+					CompressionAlgorithm: &compression.Gzip,
+					CryptoOperation:      types.Encrypt,
+				},
+				{
+					Digest:               "sha256:cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
+					Size:                 73109,
+					Annotations:          map[string]string{"org.opencontainers.image.enc.…": "layer2"},
+					MediaType:            imgspecv1.MediaTypeImageLayer,
+					CompressionOperation: types.Compress,
+					CompressionAlgorithm: &compression.Gzip,
+					CryptoOperation:      types.Encrypt,
+				},
+			},
+			expectedFixture: "ociv1.encrypted.manifest.json",
+		},
+		{
+			name:          "gzip encrypted → uncompressed decrypted",
+			sourceFixture: "ociv1.encrypted.manifest.json",
+			updates: []types.BlobInfo{
+				{
+					Digest:               "sha256:e692418e4cbaf90ca69d05a66403747baa33ee08806650b51fab815ad7fc331f",
+					Size:                 32654,
+					MediaType:            "application/vnd.oci.image.layer.v1.tar+gzip+encrypted",
+					CompressionOperation: types.Decompress,
+					CryptoOperation:      types.Decrypt,
+				},
+				{
+					Digest:               "sha256:3c3a4604a545cdc127456d94e421cd355bca5b528f4a9c1905b15da2eb4a4c6b",
+					Size:                 16724,
+					MediaType:            "application/vnd.oci.image.layer.v1.tar+gzip+encrypted",
+					CompressionOperation: types.Decompress,
+					CryptoOperation:      types.Decrypt,
+				},
+				{
+					Digest:               "sha256:ec4b8955958665577945c89419d1af06b5f7636b4ac3da7f12184802ad867736",
+					Size:                 73109,
+					MediaType:            "application/vnd.oci.image.layer.v1.tar+gzip+encrypted",
+					CompressionOperation: types.Decompress,
+					CryptoOperation:      types.Decrypt,
+				},
+			},
+			expectedFixture: "ociv1.uncompressed.manifest.json",
+		},
 	} {
 		manifest := manifestOCI1FromFixture(t, c.sourceFixture)
 
