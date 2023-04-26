942cb5c134a96754757120809a1ca1e9425c0065
Miloslav Trmač <mitr@redhat.com>
2023-04-26 02:03:42 +0200

    Reject unsupported encryption/decryption updates in UpdateLayerInfos
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- manifest/docker_schema2_test.go
+++ manifest/docker_schema2_test.go
@@ -179,6 +179,68 @@ func TestSchema2UpdateLayerInfos(t *testing.T) {
 			},
 			expectedFixture: "v2s2.nondistributable.manifest.json",
 		},
+		{
+			name:          "uncompressed → gzip encrypted",
+			sourceFixture: "v2s2.uncompressed.manifest.json",
+			updates: []types.BlobInfo{
+				{
+					Digest:               "sha256:aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+					Size:                 32654,
+					Annotations:          map[string]string{"org.opencontainers.image.enc.…": "layer1"},
+					MediaType:            DockerV2SchemaLayerMediaTypeUncompressed,
+					CompressionOperation: types.Compress,
+					CompressionAlgorithm: &compression.Gzip,
+					CryptoOperation:      types.Encrypt,
+				},
+				{
+					Digest:               "sha256:bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+					Size:                 16724,
+					Annotations:          map[string]string{"org.opencontainers.image.enc.…": "layer2"},
+					MediaType:            DockerV2SchemaLayerMediaTypeUncompressed,
+					CompressionOperation: types.Compress,
+					CompressionAlgorithm: &compression.Gzip,
+					CryptoOperation:      types.Encrypt,
+				},
+				{
+					Digest:               "sha256:cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
+					Size:                 73109,
+					Annotations:          map[string]string{"org.opencontainers.image.enc.…": "layer2"},
+					MediaType:            DockerV2SchemaLayerMediaTypeUncompressed,
+					CompressionOperation: types.Compress,
+					CompressionAlgorithm: &compression.Gzip,
+					CryptoOperation:      types.Encrypt,
+				},
+			},
+			expectedFixture: "", // Encryption is not supported
+		},
+		{
+			name:          "gzip  → uncompressed decrypted", // We can’t represent encrypted images anyway, but verify that we reject decryption attempts.
+			sourceFixture: "v2s2.manifest.json",
+			updates: []types.BlobInfo{
+				{
+					Digest:               "sha256:e692418e4cbaf90ca69d05a66403747baa33ee08806650b51fab815ad7fc331f",
+					Size:                 32654,
+					MediaType:            DockerV2Schema2LayerMediaType,
+					CompressionOperation: types.Decompress,
+					CryptoOperation:      types.Decrypt,
+				},
+				{
+					Digest:               "sha256:3c3a4604a545cdc127456d94e421cd355bca5b528f4a9c1905b15da2eb4a4c6b",
+					Size:                 16724,
+					MediaType:            DockerV2Schema2LayerMediaType,
+					CompressionOperation: types.Decompress,
+					CryptoOperation:      types.Decrypt,
+				},
+				{
+					Digest:               "sha256:ec4b8955958665577945c89419d1af06b5f7636b4ac3da7f12184802ad867736",
+					Size:                 73109,
+					MediaType:            DockerV2Schema2LayerMediaType,
+					CompressionOperation: types.Decompress,
+					CryptoOperation:      types.Decrypt,
+				},
+			},
+			expectedFixture: "", // Decryption is not supported
+		},
 	} {
 		manifest := manifestSchema2FromFixture(t, c.sourceFixture)
 
