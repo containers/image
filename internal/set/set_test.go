738c3489ce54b9b365a92431402044b996ec57cc
Miloslav Trmač <mitr@redhat.com>
2023-05-10 19:38:39 +0200

    Use a pointer receiver for internal/set.Set
    ... to make sure that, and clear to callers that, Add() and Delete()
    modify the original object and not a copy.
    
    Luckily(?), this has worked fine, because Set contains a map[],
    which is a reference type, so a copy of Set just creates another
    reference to the same map.
    
    Either way, fix the method receivers, and add tests to ensure
    the code works as expected.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

---
+++ internal/set/set_test.go
@@ -0,0 +1,66 @@
+package set
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	s := New[int]()
+	assert.True(t, s.Empty())
+}
+
+func TestNewWithValues(t *testing.T) {
+	s := NewWithValues(1, 3)
+	assert.True(t, s.Contains(1))
+	assert.False(t, s.Contains(2))
+	assert.True(t, s.Contains(3))
+}
+
+func TestAdd(t *testing.T) {
+	s := NewWithValues(1)
+	assert.False(t, s.Contains(2))
+	s.Add(2)
+	assert.True(t, s.Contains(2))
+	s.Add(2) // Adding an already-present element
+	assert.True(t, s.Contains(2))
+	// Unrelated elements are unaffected
+	assert.True(t, s.Contains(1))
+	assert.False(t, s.Contains(3))
+}
+
+func TestDelete(t *testing.T) {
+	s := NewWithValues(1, 2)
+	assert.True(t, s.Contains(2))
+	s.Delete(2)
+	assert.False(t, s.Contains(2))
+	s.Delete(2) // Deleting a missing element
+	assert.False(t, s.Contains(2))
+	// Unrelated elements are unaffected
+	assert.True(t, s.Contains(1))
+}
+
+func TestContains(t *testing.T) {
+	s := NewWithValues(1, 2)
+	assert.True(t, s.Contains(1))
+	assert.True(t, s.Contains(2))
+	assert.False(t, s.Contains(3))
+}
+
+func TestEmpty(t *testing.T) {
+	s := New[int]()
+	assert.True(t, s.Empty())
+	s.Add(1)
+	assert.False(t, s.Empty())
+	s.Delete(1)
+	assert.True(t, s.Empty())
+}
+
+func TestValues(t *testing.T) {
+	s := New[int]()
+	assert.Empty(t, s.Values())
+	s.Add(1)
+	s.Add(2)
+	assert.ElementsMatch(t, []int{1, 2}, s.Values())
+}
