1154668caa80dbef098d23e48ca6a65862de7fc4
Miloslav Trmač <mitr@redhat.com>
2023-07-19 02:53:13 +0200

    Return a new struct copySingleImageResult from copySingleImage
    ... so that we don't have so many unnamed return values, and
    we can manage the return values as a batch.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- copy/multiple.go
+++ copy/multiple.go
@@ -128,7 +128,7 @@ func (c *copier) copyMultipleImages(ctx context.Context) (copiedManifest []byte,
 			logrus.Debugf("Copying instance %s (%d/%d)", instance.sourceDigest, i+1, len(instanceCopyList))
 			c.Printf("Copying image %s (%d/%d)\n", instance.sourceDigest, i+1, len(instanceCopyList))
 			unparsedInstance := image.UnparsedInstance(c.rawSource, &instanceCopyList[i].sourceDigest)
-			updatedManifest, updatedManifestType, updatedManifestDigest, err := c.copySingleImage(ctx, unparsedInstance, &instanceCopyList[i].sourceDigest)
+			updated, err := c.copySingleImage(ctx, unparsedInstance, &instanceCopyList[i].sourceDigest)
 			if err != nil {
 				return nil, fmt.Errorf("copying image %d/%d from manifest list: %w", i+1, len(instanceCopyList), err)
 			}
@@ -136,9 +136,9 @@ func (c *copier) copyMultipleImages(ctx context.Context) (copiedManifest []byte,
 			instanceEdits = append(instanceEdits, internalManifest.ListEdit{
 				ListOperation:   internalManifest.ListOpUpdate,
 				UpdateOldDigest: instance.sourceDigest,
-				UpdateDigest:    updatedManifestDigest,
-				UpdateSize:      int64(len(updatedManifest)),
-				UpdateMediaType: updatedManifestType})
+				UpdateDigest:    updated.manifestDigest,
+				UpdateSize:      int64(len(updated.manifest)),
+				UpdateMediaType: updated.manifestMIMEType})
 		default:
 			return nil, fmt.Errorf("copying image: invalid copy operation %d", instance.op)
 		}
