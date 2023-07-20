54d86a5215a2b1467c008a3d84e7399a665b67f9
Aditya R <arajan@redhat.com>
2023-07-20 11:11:23 +0530

    copy/single: accept requireCompressionFormatMatch for imagecopier
    imagecopier implementes feature for requireCompressionFormatMatch
    refactor `copySingleImage` to accept that as an argument.
    
    Signed-off-by: Aditya R <arajan@redhat.com>

--- copy/copy.go
+++ copy/copy.go
@@ -251,7 +251,7 @@ func Image(ctx context.Context, policyContext *signature.PolicyContext, destRef,
 
 	if !multiImage {
 		// The simple case: just copy a single image.
-		single, err := c.copySingleImage(ctx, c.unparsedToplevel, nil)
+		single, err := c.copySingleImage(ctx, c.unparsedToplevel, nil, false)
 		if err != nil {
 			return nil, err
 		}
@@ -274,7 +274,7 @@ func Image(ctx context.Context, policyContext *signature.PolicyContext, destRef,
 		logrus.Debugf("Source is a manifest list; copying (only) instance %s for current system", instanceDigest)
 		unparsedInstance := image.UnparsedInstance(rawSource, &instanceDigest)
 
-		single, err := c.copySingleImage(ctx, unparsedInstance, nil)
+		single, err := c.copySingleImage(ctx, unparsedInstance, nil, false)
 		if err != nil {
 			return nil, fmt.Errorf("copying system image from manifest list: %w", err)
 		}
