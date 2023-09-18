31d959e1e44a2c23199dfae42957267b29f494f1
Colin Walters <walters@verbum.org>
2023-09-18 09:58:28 -0400

    ociarchive: Add new `ArchiveFileNotFoundError`
    This is for https://github.com/containers/skopeo/pull/2114
    which is in turn a dependency of https://github.com/coreos/rpm-ostree/pull/4598
    
    Basically I want to map ENOENT to a clear error, because the build
    tooling wants to treat "target image is not present" differently
    from "DNS lookup failed" or "we got EPERM".
    
    There's a bit of code motion here because we need to move
    the `os.Open()` call before creating a temporary directory.
    
    Signed-off-by: Colin Walters <walters@verbum.org>

--- oci/archive/oci_src_test.go
+++ oci/archive/oci_src_test.go
@@ -1,5 +1,26 @@
 package archive
 
-import "github.com/containers/image/v5/internal/private"
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/containers/image/v5/internal/private"
+	"github.com/containers/image/v5/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 var _ private.ImageSource = (*ociArchiveImageSource)(nil)
+
+func TestNewImageSourceNotFound(t *testing.T) {
+	sysctx := types.SystemContext{}
+	emptyDir := t.TempDir()
+	archivePath := filepath.Join(emptyDir, "foo.ociarchive")
+	imgref, err := ParseReference(archivePath)
+	require.NoError(t, err)
+	_, err = LoadManifestDescriptorWithContext(&sysctx, imgref)
+	assert.NotNil(t, err)
+	var aerr ArchiveFileNotFoundError
+	assert.ErrorAs(t, err, &aerr)
+	assert.Equal(t, aerr.path, archivePath)
+}
