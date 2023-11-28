7e61d1e45bc3ff91b9b29caf4b2f08f51a2c1ff2
Miloslav Trmač <mitr@redhat.com>
2023-11-28 01:36:30 +0100

    Don't expose local account details in oci-archive tar files
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- oci/archive/oci_dest_test.go
+++ oci/archive/oci_dest_test.go
@@ -1,5 +1,45 @@
 package archive
 
-import "github.com/containers/image/v5/internal/private"
+import (
+	"archive/tar"
+	"io"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/containers/image/v5/internal/private"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 var _ private.ImageDestination = (*ociArchiveImageDestination)(nil)
+
+func TestTarDirectory(t *testing.T) {
+	srcDir := t.TempDir()
+	err := os.WriteFile(filepath.Join(srcDir, "regular"), []byte("contents"), 0o600)
+	require.NoError(t, err)
+
+	dest := filepath.Join(t.TempDir(), "file.tar")
+	err = tarDirectory(srcDir, dest)
+	require.NoError(t, err)
+
+	f, err := os.Open(dest)
+	require.NoError(t, err)
+	defer f.Close()
+	reader := tar.NewReader(f)
+	numItems := 0
+	for {
+		hdr, err := reader.Next()
+		if err == io.EOF {
+			break
+		}
+		require.NoError(t, err)
+		// Test that the header does not expose data about the local account
+		assert.Equal(t, 0, hdr.Uid)
+		assert.Equal(t, 0, hdr.Gid)
+		assert.Empty(t, hdr.Uname)
+		assert.Empty(t, hdr.Gname)
+		numItems++
+	}
+	assert.Equal(t, 1, numItems)
+}
