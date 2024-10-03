985156155293f566a342610710653c0a923d9a32
Giuseppe Scrivano <gscrivan@redhat.com>
2024-10-03 14:18:55 +0200

    image: define error for partial pulls not available
    define a new error type so that the caller can determine whether it is
    safe to ignore the error and retrieve the resource fully.
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- storage/storage_dest.go
+++ storage/storage_dest.go
@@ -311,8 +311,9 @@ func (f *zstdFetcher) GetBlobAt(chunks []chunked.ImageSourceChunk) (chan io.Read
 // PutBlobPartial attempts to create a blob using the data that is already present
 // at the destination. chunkAccessor is accessed in a non-sequential way to retrieve the missing chunks.
 // It is available only if SupportsPutBlobPartial().
-// Even if SupportsPutBlobPartial() returns true, the call can fail, in which case the caller
-// should fall back to PutBlobWithOptions.
+// Even if SupportsPutBlobPartial() returns true, the call can fail.
+// If the call fails with ErrFallbackToOrdinaryLayerDownload, the caller can fall back to PutBlobWithOptions.
+// The fallback _must not_ be done otherwise.
 func (s *storageImageDestination) PutBlobPartial(ctx context.Context, chunkAccessor private.BlobChunkAccessor, srcInfo types.BlobInfo, options private.PutBlobPartialOptions) (private.UploadedBlob, error) {
 	fetcher := zstdFetcher{
 		chunkAccessor: chunkAccessor,
