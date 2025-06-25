ea5c252e53fdb45faa8b8e26818ce2125072e7e0
Miloslav Trmač <mitr@redhat.com>
2025-06-25 21:32:32 +0200

    Add a ~representative test of the typical workflow to sequoia.SigningMechanism
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- signature/internal/sequoia/sequoia_test.go
+++ signature/internal/sequoia/sequoia_test.go
@@ -6,12 +6,15 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
-	"github.com/containers/image/v5/signature/internal/sequoia"
 	"io"
 	"os"
 	"os/exec"
 	"regexp"
 	"testing"
+
+	"github.com/containers/image/v5/signature/internal/sequoia"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func checkCliVersion(version string) error {
@@ -216,6 +219,38 @@ func TestImportSignVerifyEphemeral(t *testing.T) {
 	if keyIdentity != fingerprint {
 		t.Fatalf("keyIdentity differ from the original")
 	}
+}
+
+func TestSignThenVerifyEphemeral(t *testing.T) {
+	if err := checkCliVersion("1.3.0"); err != nil {
+		t.Skipf("sq not usable: %v", err)
+	}
+	dir := t.TempDir()
+	fingerprint, err := generateKey(dir, "foo@example.org")
+	require.NoError(t, err)
+	publicKey, err := exportCert(dir, fingerprint)
+	require.NoError(t, err)
+	m1, err := sequoia.NewMechanismFromDirectory(dir)
+	require.NoError(t, err)
+
+	input := []byte("Hello, world!")
+	sig, err := m1.Sign(input, fingerprint)
+	require.NoError(t, err)
+
+	m2, err := sequoia.NewEphemeralMechanism()
+	require.NoError(t, err)
+
+	_, _, err = m2.Verify(sig) // With no public key, verification should fail
+	assert.Error(t, err)
+
+	keyIdentities, err := m2.ImportKeys(publicKey)
+	require.NoError(t, err)
+	require.Len(t, keyIdentities, 1)
+	require.Equal(t, fingerprint, keyIdentities[0])
+	contents, keyIdentity, err := m2.Verify(sig)
+	require.NoError(t, err)
+	assert.Equal(t, input, contents)
+	assert.Equal(t, keyIdentity, fingerprint)
 }
 
 func TestMain(m *testing.M) {
