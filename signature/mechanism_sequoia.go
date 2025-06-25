5cc60e1655a96b75054bc04527ecc16b65a59a6a
Miloslav Trmač <mitr@redhat.com>
2025-06-25 22:59:05 +0200

    With sequoia, still use GPGME for existing signing, and add a new Signer API
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- signature/mechanism_sequoia.go
+++ signature/mechanism_sequoia.go
@@ -3,70 +3,15 @@
 package signature
 
 import (
-	"os"
-	"path"
-
 	"github.com/containers/image/v5/signature/internal/sequoia"
-	"github.com/containers/storage/pkg/homedir"
 )
 
-// A GPG/OpenPGP signing mechanism, implemented using Sequoia.
-type sequoiaSigningMechanism struct {
+// A GPG/OpenPGP signing mechanism, implemented using Sequoia and only supporting verification.
+// Legacy users who reach newGPGSigningMechanismInDirectory will use GPGME.
+// Signing using Sequoia is preferable, but should happen via signature/simplesequoia.NewSigner, not using
+// the legacy mechanism API.
+type sequoiaEphemeralSigningMechanism struct {
 	inner *sequoia.SigningMechanism
-}
-
-// newGPGSigningMechanismInDirectory returns a new GPG/OpenPGP signing mechanism, using optionalDir if not empty.
-// The caller must call .Close() on the returned SigningMechanism.
-func newGPGSigningMechanismInDirectory(optionalDir string) (signingMechanismWithPassphrase, error) {
-	if err := sequoia.Init(); err != nil {
-		return nil, err
-	}
-
-	// For compatibility reasons, we allow both sequoiaHome and
-	// gpgHome to be the same directory as designated by
-	// optionalDir or GNUPGHOME.
-	envHome := os.Getenv("GNUPGHOME")
-
-	gpgHome := optionalDir
-	if gpgHome == "" {
-		gpgHome = envHome
-		if gpgHome == "" {
-			gpgHome = path.Join(homedir.Get(), ".gnupg")
-		}
-	}
-
-	sequoiaHome := optionalDir
-	if sequoiaHome == "" {
-		sequoiaHome = envHome
-		if sequoiaHome == "" {
-			dataHome, err := homedir.GetDataHome()
-			if err != nil {
-				return nil, err
-			}
-			sequoiaHome = path.Join(dataHome, "sequoia")
-		}
-	}
-
-	mech, err := sequoia.NewMechanismFromDirectory(sequoiaHome)
-	if err != nil {
-		return nil, err
-	}
-
-	// Migrate GnuPG keyrings if exist.
-	for _, keyring := range []string{"pubring.gpg", "secring.gpg"} {
-		blob, err := os.ReadFile(path.Join(gpgHome, keyring))
-		if err != nil {
-			if !os.IsNotExist(err) {
-				return nil, err
-			}
-		} else if _, err := mech.ImportKeys(blob); err != nil {
-			return nil, err
-		}
-	}
-
-	return &sequoiaSigningMechanism{
-		inner: mech,
-	}, nil
 }
 
 // newEphemeralGPGSigningMechanism returns a new GPG/OpenPGP signing mechanism which
@@ -91,37 +36,41 @@ func newEphemeralGPGSigningMechanism(blobs [][]byte) (signingMechanismWithPassph
 		keyIdentities = append(keyIdentities, ki...)
 	}
 
-	return &sequoiaSigningMechanism{
+	return &sequoiaEphemeralSigningMechanism{
 		inner: mech,
 	}, keyIdentities, nil
 }
 
-func (m *sequoiaSigningMechanism) Close() error {
+func (m *sequoiaEphemeralSigningMechanism) Close() error {
 	return m.inner.Close()
 }
 
 // SupportsSigning returns nil if the mechanism supports signing, or a SigningNotSupportedError.
-func (m *sequoiaSigningMechanism) SupportsSigning() error {
-	return nil
+func (m *sequoiaEphemeralSigningMechanism) SupportsSigning() error {
+	// This code is externally reachable via NewEphemeralGPGSigningMechanism(), but that API provides no way to
+	// import or generate a key.
+	return SigningNotSupportedError("caller error: Attempt to sign using a mechanism created via NewEphemeralGPGSigningMechanism().")
 }
 
 // Sign creates a (non-detached) signature of input using keyIdentity and passphrase.
 // Fails with a SigningNotSupportedError if the mechanism does not support signing.
-func (m *sequoiaSigningMechanism) SignWithPassphrase(input []byte, keyIdentity string, passphrase string) ([]byte, error) {
-	return m.inner.SignWithPassphrase(input, keyIdentity, passphrase)
+func (m *sequoiaEphemeralSigningMechanism) SignWithPassphrase(input []byte, keyIdentity string, passphrase string) ([]byte, error) {
+	// This code is externally reachable via NewEphemeralGPGSigningMechanism(), but that API provides no way to
+	// import or generate a key.
+	return nil, SigningNotSupportedError("caller error: Attempt to sign using a mechanism created via NewEphemeralGPGSigningMechanism().")
 }
 
 // Sign creates a (non-detached) signature of input using keyIdentity.
 // Fails with a SigningNotSupportedError if the mechanism does not support signing.
-func (m *sequoiaSigningMechanism) Sign(input []byte, keyIdentity string) ([]byte, error) {
-	return m.inner.Sign(input, keyIdentity)
+func (m *sequoiaEphemeralSigningMechanism) Sign(input []byte, keyIdentity string) ([]byte, error) {
+	return m.SignWithPassphrase(input, keyIdentity, "")
 }
 
 // Verify parses unverifiedSignature and returns the content and the signer's identity.
 // For mechanisms created using NewEphemeralGPGSigningMechanism, the returned key identity
 // is expected to be one of the values returned by NewEphemeralGPGSigningMechanism,
 // or the mechanism should implement signingMechanismWithVerificationIdentityLookup.
-func (m *sequoiaSigningMechanism) Verify(unverifiedSignature []byte) (contents []byte, keyIdentity string, err error) {
+func (m *sequoiaEphemeralSigningMechanism) Verify(unverifiedSignature []byte) (contents []byte, keyIdentity string, err error) {
 	return m.inner.Verify(unverifiedSignature)
 }
 
@@ -130,6 +79,6 @@ func (m *sequoiaSigningMechanism) Verify(unverifiedSignature []byte) (contents [
 // WARNING: The short key identifier (which corresponds to "Key ID" for OpenPGP keys)
 // is NOT the same as a "key identity" used in other calls to this interface, and
 // the values may have no recognizable relationship if the public key is not available.
-func (m *sequoiaSigningMechanism) UntrustedSignatureContents(untrustedSignature []byte) (untrustedContents []byte, shortKeyIdentifier string, err error) {
+func (m *sequoiaEphemeralSigningMechanism) UntrustedSignatureContents(untrustedSignature []byte) (untrustedContents []byte, shortKeyIdentifier string, err error) {
 	return gpgUntrustedSignatureContents(untrustedSignature)
 }
